6f54c622cc9876d2a29d54d4008fb2a1e3139f1a
jcamhi <jeremshy@h3lp.fr>
2017-05-25 20:17:33 +0200

    Rainbow Dash is best pony !

--- src/Polynome.java
+++ src/Polynome.java
@@ -1,11 +1,42 @@
 import java.util.ArrayList;
 
 public class Polynome {
+
 	private ArrayList<Monome> pol;
 
 	Polynome() {
 		pol = new ArrayList<Monome>();
 	}
+
+	Polynome (String arg) {
+		this();
+		
+		int 	signe;
+		double	coefficient;
+		int		puissance;
+		int		i;
+		int		len;
+
+		i = 0;
+			while (i < arg.length()) {
+				System.out.println("[" + arg.substring(i) + "]");
+				if (arg.charAt(i) == '-')
+				{
+					signe = -1;
+					i++;
+				}
+				else
+					signe = 1;
+				len = 0;
+				while (Character.isDigit(arg.charAt(i + len)) || arg.charAt(i + len) == '.') {
+					len++;
+				}
+				coefficient = Double.parseDouble(arg.substring(i, i + len)) * signe;
+				System.out.println("Coefficient: " + coefficient);
+				i += len;
+			}
+		}
+	
 
 	public void add_monome(Monome to_add) {
 		Monome	i;
--- src/computor.java
+++ src/computor.java
@@ -1,11 +1,47 @@
  public class computor {
+	 
 
 	public static void main(String[] args) {
-		String arg;
+		Polynome pol;
 		
-		if (args.length != 2) {
-			System.out.println("Wrong number of arguments :/");
+		if (args.length != 1) {
+			System.out.println("Wrong number of arguments :( ");
 		}
+		
+		System.out.println("Argument : [" + args[0] + "]");
+		try {
+			pol = new Polynome(args[0]);
+		} catch (Exception e) {
+			System.out.println("Une erreur est survenue lors du parsing. Veuillez vérifier le bon format de l'expression entrée. "
+					+ "Détails de l'erreur : " + e.toString() + ".");
+			System.out.println("Computor-V1 va à présent se terminer.");
+		}
+		//		arg = args[0];
+//		System.out.println("Parsing arg : [" + arg + "]");
+//		i = 0;
+//		while(arg.charAt(i) == ' ')
+//			i++;
+//		if (arg.charAt(i) == '-') {
+//			signe = -1;
+//			i++;
+//		}
+//		else
+//			signe = 1;
+//		len = 0;
+//		while (Character.isDigit(arg.charAt(i + len)) || arg.charAt(i + len) == '.') {
+//			len++;
+//		}
+//		coef = Double.parseDouble(arg.substring(i, i + len)) * signe;
+//		System.out.println("Coef : " + coef);
+//		i = i + len;
+//		i = arg.indexOf('X', i);
+//		while(arg.charAt(i) == ' ')
+//			i++;
+//		if (arg.charAt(i) == '-') {
+//			signe = -1;
+//			i++;
+//		}
+//		else
+//			signe = 1;
 	}
-
 }
